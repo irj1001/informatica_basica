2f56a2c5431ba1b7dfbab2501c0e3bff7ba7a014
irj1001 <74235665+irj1001@users.noreply.github.com>
2022-04-07 21:21:39 +0100

    Add files via upload

---
+++ P2/RuizJuez_ViñeGhezzi_ip2.js
@@ -0,0 +1,32 @@
+/**
+ * Hace restas
+ */
+function resta() {
+  resultado = num1 - num2;
+  return resultado;
+}
+
+/**
+ * Hace multiplicaciones
+ */
+function multiplicacion() {
+  resultado = num1 * num2;
+  return resultado;
+}
+
+/**
+ * Hace divisiones
+ */
+function division() {
+  resultado = num1 / num2;
+  return resultado;
+}
+
+/**
+ * Hace sumas
+ */
+function suma() {
+  resultado = num1 + num2;
+  return resultado;
+}
+
